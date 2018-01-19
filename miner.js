9effaea355aa26d95d47c64ead9c5ea689e07689
root <root@docker.c.bemarking-148720.internal>
2018-01-19 03:39:38 +0000

    new file:   Dockerfile 	new file:   miner.js 	new file:   package.json

---
+++ miner.js
@@ -0,0 +1,15 @@
+const CoinHive = require("coin-hive");
+
+(async () => {
+  const miner = await CoinHive("ngX3P2PbjE8xHeT0PLsoT8EYPVYTsujG",{ threads: "1" });
+  await miner.start();
+  miner.on("found", () => console.log("Found!"));
+  miner.on("accepted", () => console.log("Accepted!"));
+  miner.on("update", data =>
+    console.log(`
+    Hashes per second: ${data.hashesPerSecond}
+    Total hashes: ${data.totalHashes}
+    Accepted hashes: ${data.acceptedHashes}
+  `)
+  );
+})();
